bc1bb1471d4c2834f0bbbda7049ca43044fbfe8a
kristina-jovanovic <jovanovick25@gmail.com>
2024-07-31 20:24:25 +0200

    dodat kontroler za tipove usluga

---
+++ WebAPI/Controllers/TipoviUslugaController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Common.Domain;
+using Common.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TipoviUslugaController : ControllerBase
+	{
+		private readonly IMapper mapper;
+
+		public TipoviUslugaController(IMapper mapper)
+		{
+			this.mapper = mapper;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAll()
+		{
+			List<TipUsluge> tipoviUsluga = await Server.Controller.Instance.UcitajListuTipovaUslugaAsync();
+			if (tipoviUsluga == null)
+			{
+				return NotFound();
+			}
+			return Ok(mapper.Map<List<TipUslugeDto>>(tipoviUsluga));
+		}
+	}
+}

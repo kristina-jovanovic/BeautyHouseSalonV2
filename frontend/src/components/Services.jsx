d2cb41c8f0202ca4d3fac42782dcb284a85b4b7e
kristina-jovanovic <jovanovick25@gmail.com>
2024-08-20 16:20:37 +0200

    Dodata pretraga za usluge

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -3,9 +3,10 @@ import OneService from './OneService'
 import Loader from './Loader'
 import axios from 'axios';
 
-function Services({addService}) {
+function Services({ addService }) {
     const [loading, setLoading] = useState(true);
 
+    const [services, setServices] = useState();
     const [serviceChunks, setServiceChunks] = useState();
 
     // Function to split services into groups of six
@@ -17,15 +18,44 @@ function Services({addService}) {
         return result;
     };
 
-    const [services, setServices] = useState();
-    useEffect(() => {
-        if (services == null) {
+    function vratiSveUsluge() {
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: 'api/Usluge'
+        };
+
+        axios.request(config)
+            .then((response) => {
+                // console.log(JSON.stringify(response.data));
+                setServices(response.data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    const [filter, setFilter] = useState('');
+    function handleInput(e) {
+        let newFilter = filter;
+        newFilter = e.target.value;
+        // console.log("newFilter "+ newFilter);
+        // console.log("filter "+ filter);
+        setFilter(newFilter);
+        setLoading(true);
+
+        e.preventDefault();
+        if (newFilter === '') {
+            vratiSveUsluge();
+        }
+        else{
             let config = {
                 method: 'get',
                 maxBodyLength: Infinity,
-                url: 'api/Usluge'
+                url: `api/Usluge?filterQuery=${newFilter}`
             };
-
+    
             axios.request(config)
                 .then((response) => {
                     // console.log(JSON.stringify(response.data));
@@ -35,6 +65,12 @@ function Services({addService}) {
                 .catch((error) => {
                     console.log(error);
                 });
+        }
+    }
+
+    useEffect(() => {
+        if (services == null) { 
+            vratiSveUsluge();
         }
         else {
             setServiceChunks(chunkServices(services, 6));
@@ -49,6 +85,11 @@ function Services({addService}) {
                     <h2 className="section-heading text-uppercase">Usluge</h2>
                     <h3 className="section-subheading text-muted">Izaberi i zakaži tretman za sebe.</h3>
                 </div>
+                <form className="d-flex" style={{ marginTop: '1rem', marginBottom: '1rem', alignItems:'center',justifyContent:'center' }}>
+                    <input className="form-control search" type="search" placeholder="Pretraži usluge po nazivu ili tipu usluge" aria-label="Search"
+                        onInput={handleInput} name='filter' />
+                    <button type="submit" className="btn btn-outline-primary" disabled><i className="fa-solid fa-magnifying-glass"></i></button>
+                </form>
                 <div id="carouselExampleIndicators" className="carousel slide">
                     <div className="carousel-indicators" style={{ marginBottom: 0 }}>
                         {serviceChunks?.map((chunk, index) => (
